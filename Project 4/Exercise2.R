e3d54b72cb725c85b2c710e88f43a389bda2e5f0
ekalgolas <exg140230@utdallas.edu>
2015-11-12 17:30:13 -0600

    fixing project 4

--- Project 4/Exercise2.R
+++ Project 4/Exercise2.R
@@ -5,28 +5,19 @@ sample.size = 20
 H0.mean = 10
 
 # Compute test statistic
-test = (H0.mean - sample.mean) / (sample.sd / sqrt(sample.size))
+test = (-H0.mean + sample.mean) / (sample.sd / sqrt(sample.size))
 test
 
 # Get the p-value using t distribution
-p.value = (1 - pt(abs(test), sample.size - 1))
+p.value = (1 - pt(test, sample.size - 1))
 p.value
 
 # Get p-value using monte carlo simulation
 p.value.sim = function(){
-  count = 0
-  for (i in 1:1000){
     x = rnorm(sample.size, mean = sample.mean, sd = sample.sd)
-    if (mean(x) > H0.mean) {
-      count = count + 1
-    }
-  }
-  
-  return (count / 1000)
+    mean(x)
 }
 
 # Simulate 100 times
-sim.pvalue = replicate(100, p.value.sim())
-
-# State at 5% significance
-sum(sim.pvalue) / 100
+sim = replicate(10000, p.value.sim())
+1 - length(sim[sim> H0.mean]) / 10000
