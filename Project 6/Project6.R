7a2f06e520f712b503854f32cb7b861fe609742f
ekalgolas <exg140230@utdallas.edu>
2015-12-03 17:58:37 -0600

    Modifying project 6

--- Project 6/Project6.R
+++ Project 6/Project6.R
@@ -2,29 +2,35 @@
 cancer.data = read.table("prostate_cancer.csv", sep = ",", header = TRUE)
 
 # Fit the model with two variables
-fit = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + (cancer.data$cancervol), cancer.data)
-summary(fit)
+fit1 = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + (cancer.data$cancervol), cancer.data)
+fit2 = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + (cancer.data$age), cancer.data)
+fit3 = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + (cancer.data$benpros), cancer.data)
+fit4 = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + (cancer.data$vesinv), cancer.data)
+fit5 = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + (cancer.data$capspen), cancer.data)
+fit6 = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + factor(cancer.data$gleason), cancer.data)
+anova(fit1, fit2, fit3, fit4, fit5, fit6)
+summary(fit4)
 
 # Residual plot
 jpeg("Residual plot.jpg", width = 1080, height = 480)
-plot(fitted(fit), resid(fit), main = "Residual plot")
+plot(fitted(fit4), resid(fit4), main = "Residual plot")
 abline(h=0)
 dev.off()
 
 # QQ plot
 jpeg("QQ Plot.jpg", width = 1080, height = 480)
-qqnorm(resid(fit), xlim = c(-3, 4))
-qqline(resid(fit))
+qqnorm(resid(fit4), xlim = c(-3, 4))
+qqline(resid(fit4))
 dev.off()
 
 # Time series plot of residuals
 jpeg("Time series plot.jpg", width = 1080, height = 480)
-plot(resid(fit), type="l", main = "Time series plot")
+plot(resid(fit4), type="l", main = "Time series plot")
 abline(h=0)
 dev.off
 
 # Log transform and fit the model with two variables
-final.fit = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + log(cancer.data$cancervol), cancer.data)
+final.fit = lm(log(cancer.data$psa) ~ log(cancer.data$weight) + log(cancer.data$vesinv), cancer.data)
 summary(final.fit)
 
 # Residual plot
@@ -40,15 +46,15 @@ qqline(resid(final.fit))
 dev.off()
 
 # Time series plot of residuals
-jpeg("Final - Time series plot.jpg", width = 1080, height = 480)
+jpeg("Final - Time series plot.jpg")
 plot(resid(final.fit), type="l", main = "Time series plot")
 abline(h=0)
 dev.off()
 
 # Predict for median
 weight.median = median(cancer.data$weight)
-cancervol.median = median(cancer.data$cancervol)
-index = cancer.data[which(cancer.data$weight == weight.median | cancer.data$cancervol == cancervol.median), ]
-new.data = data.frame(x = c(weight.median, cancervol.median))
+ves.median = median(cancer.data$vesinv)
+index = cancer.data[which(cancer.data$weight == weight.median & cancer.data$vesinv == ves.median), ]
+new.data = data.frame(x = c(weight.median, ves.median))
 prediction = predict(fit, newdata = new.data)
 exp(prediction[index$subject])
